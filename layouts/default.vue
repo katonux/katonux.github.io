7eb8f3d285531980bff4df45f31589e3dca26d48
katonux <sheeptech@vivaldi.net>
2020-10-03 10:34:29 +0900

    Remove: unnecessary components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list></v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-app-bar fixed app dense>
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
@@ -26,11 +16,7 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      miniVariant: false,
-      title: 'hogehoge',
+      title: 'KATONUX',
     }
   },
 }
