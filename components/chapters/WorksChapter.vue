9e1b146a402f9caeb22572987471bed0aad4d9bf
katonux <sheeptech@vivaldi.net>
2020-10-03 20:49:24 +0900

    Add: Works contents

--- components/chapters/WorksChapter.vue
+++ components/chapters/WorksChapter.vue
@@ -5,16 +5,31 @@
   >
     <div class="text-center">
       <h2>Works</h2>
-      <v-container>
-        <v-row>
-          <v-col class="d-flex justify-center">
-            <v-card width="500px">
-              <v-img src="/nemushee_header.png" />
-              <v-card-title> Nemushee.net </v-card-title>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
     </div>
+    <v-container>
+      <v-row>
+        <v-col class="d-flex justify-center" cols="12">
+          <v-card width="700px">
+            <v-img src="/nemushee_header.png" />
+            <v-card-title>Nemushee.net</v-card-title>
+            <v-card-text> Self-hosted mastodon instance </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col class="d-flex justify-center" cols="12">
+          <v-card width="700px">
+            <v-img> no image </v-img>
+            <v-card-title>katonux.github.io</v-card-title>
+            <v-card-text>This Page!</v-card-text>
+          </v-card>
+        </v-col>
+        <v-col class="d-flex justify-center" cols="12">
+          <v-card width="700px">
+            <v-img> no image </v-img>
+            <v-card-title>Spotify Nowplaying</v-card-title>
+            <v-card-text> https://spotify-nowplaying.web.app </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
