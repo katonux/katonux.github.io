851fb1fc9b9a2793bf4b364810c0a56d9d0690ac
katonux <sheeptech@vivaldi.net>
2020-10-03 12:42:55 +0900

    Improve: Deprecated property 'ssr'

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [{ src: 'plugins/vuetify.ts', ssr: false }],
+  plugins: [{ src: 'plugins/vuetify.ts', mode: 'client' }],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
