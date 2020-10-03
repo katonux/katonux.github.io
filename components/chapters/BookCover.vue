7b165912ad6ba66c9023055fa4151349e564c06a
katonux <sheeptech@vivaldi.net>
2020-10-03 14:25:10 +0900

    Add: Avatar icon

--- components/chapters/BookCover.vue
+++ components/chapters/BookCover.vue
@@ -4,8 +4,14 @@
       fluid
       style="background-image: linear-gradient(-45deg, #85a333, #0db0c9)"
     >
-      <v-row align="center" justify="center" style="height: 500px">
+      <v-row align="center" justify="center" style="height: 70vh">
         <v-col class="text-center" cols="12">
+          <v-avatar size="150" class="my-4">
+            <img
+              src="https://avatars0.githubusercontent.com/u/36836040?s=460&u=b80acba5bc20a660cb0c4edbfa042b6431fc3f2a&v=4"
+              alt="katonux"
+            />
+          </v-avatar>
           <h1 class="display-1 font-weight-thin mb-4">Katonux</h1>
           <h4 class="subheading">Shota Kato</h4>
         </v-col>
