b830ce27338abd6c720f60dde2061c71616fbe79
katonux <sheeptech@vivaldi.net>
2020-10-03 10:56:52 +0900

    Add: Navigation Drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,24 @@
       <v-toolbar-title v-text="title" />
     </v-app-bar>
     <v-main>
+      <v-navigation-drawer
+        floating
+        permanent
+        absolute
+        class="ma-10"
+        height="200"
+      >
+        <v-list dense rounded>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-check</v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
       <nuxt />
     </v-main>
-    <v-footer :absolute="!fixed" app>
+    <v-footer app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
@@ -17,6 +32,10 @@ export default {
   data() {
     return {
       title: 'KATONUX',
+      items: [
+        { title: 'Home', icon: 'mdi-view-dashboard' },
+        { title: 'About', icon: 'mdi-forum' },
+      ],
     }
   },
 }
