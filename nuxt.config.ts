ca93e9d6b0d18ae6c30650937bb2accdd6637a0a
katonux <sheeptech@vivaldi.net>
2020-10-07 22:30:47 +0900

    Update: Title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,8 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - katonux-portfolio',
-    title: 'katonux-portfolio',
+    titleTemplate: '%s - Katonux',
+    title: 'Katonux',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
